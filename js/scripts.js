32e2af1aea864f61ae374ddaa28d1aa85448d75e
mathrews <mateusps4gg@gmail.com>
2023-07-31 21:48:07 -0300

    layout finished

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,19 @@ let pomodoroMode = document.querySelector('#tempos .pomodoro');
 let shortBreakMode = document.querySelector('#tempos .shortBreak');
 let longBreakMode = document.querySelector('#tempos .longBreak');
 
-
+pomodoroMode.onclick = () => {
+    pomodoroMode.classList.add('active');
+    shortBreakMode.classList.remove('active');
+    longBreakMode.classList.remove('active');
+}
+shortBreakMode.onclick = () => {
+    shortBreakMode.classList.add('active');
+    longBreakMode.classList.remove('active');
+    pomodoroMode.classList.remove('active');
+}
+longBreakMode.onclick = () => {
+    longBreakMode.classList.add('active');
+    shortBreakMode.classList.remove('active');
+    pomodoroMode.classList.remove('active');
+}
 
