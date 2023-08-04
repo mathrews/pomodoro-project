ee5c1c70024c301588a77bb5ac42eee8b8df5609
mathrews <mateusps4gg@gmail.com>
2023-08-03 21:11:01 -0300

    faltando apenas função contar minutos e segundos, colocar a barra e colocar o alarme

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,3 @@
-let pomodoroMode = document.querySelector('#tempos .pomodoro');
-let shortBreakMode = document.querySelector('#tempos .shortBreak');
-let longBreakMode = document.querySelector('#tempos .longBreak');
-
-pomodoroMode.onclick = () => {
-    pomodoroMode.classList.add('active');
-    shortBreakMode.classList.remove('active');
-    longBreakMode.classList.remove('active');
-}
-shortBreakMode.onclick = () => {
-    shortBreakMode.classList.add('active');
-    longBreakMode.classList.remove('active');
-    pomodoroMode.classList.remove('active');
-}
-longBreakMode.onclick = () => {
-    longBreakMode.classList.add('active');
-    shortBreakMode.classList.remove('active');
-    pomodoroMode.classList.remove('active');
-}
-
 btnGear.onclick = () => {
     overlay.classList.add('activeOverlay')
     modal.classList.add('activeModal')
@@ -35,16 +15,79 @@ btnCrossModal.onclick = () => {
 
 let configDeCadaTempo = JSON.parse(localStorage.getItem('configuracaoDoTempo')) || [];
 
-function submitar(){
+function verificarExistenciaDeConfigs() {
+    if (configDeCadaTempo.length == 1) {
+        configDeCadaTempo = [];
+    }
+    localStorage.setItem('configuraçaoDoTempo', JSON.stringify(configDeCadaTempo));
+}
+
+function submitarFormConfigsDeTempo(){
     event.preventDefault();
+    verificarExistenciaDeConfigs();
     let configDoTempo = {
         tempoPomodoro: Number(pomodoroMinutes.value),
         tempoShort: Number(shortBreakMinutes.value),
         tempoLong: Number(longBreakMinutes.value)
     };
     configDeCadaTempo.push(configDoTempo);
+    formDosTempos.reset();
     localStorage.setItem('configuraçaoDoTempo', JSON.stringify(configDeCadaTempo));
+    pomodoroMode();
     overlay.classList.remove('activeOverlay');
     modal.classList.remove('activeModal');
-    console.log(configDoTempo);
+}
+
+function setarTempo(minutosParam) {
+    if (minutosParam) {
+        if (minutosParam < 10 && minutosParam >= 1) {
+            minuto.innerHTML = `0${minutosParam}`;
+        }
+        else {
+            minuto.innerHTML = `${minutosParam}`;
+        }
+    }
+}
+
+function pomodoroMode(){
+    pomodoro.classList.add('active');
+    shortBreak.classList.remove('active');
+    longBreak.classList.remove('active');
+    if (configDeCadaTempo[0].tempoPomodoro == 0) {
+        minuto.innerHTML = `00`;
+    }
+    setarTempo(configDeCadaTempo[0].tempoPomodoro);
+}
+
+function shortBreakMode(){
+    shortBreak.classList.add('active');
+    longBreak.classList.remove('active');
+    pomodoro.classList.remove('active');
+    if (configDeCadaTempo[0].tempoShort == 0) {
+        minuto.innerHTML = `00`;
+    }
+    setarTempo(configDeCadaTempo[0].tempoShort);
+}
+
+function longBreakMode(){
+    longBreak.classList.add('active');
+    shortBreak.classList.remove('active');
+    pomodoro.classList.remove('active');
+    if (configDeCadaTempo[0].tempoLong == 0) {
+        minuto.innerHTML = `00`;
+    }
+    setarTempo(configDeCadaTempo[0].tempoLong);
+}
+
+botaoPlay.onclick = () => {
+    switch (botaoPlay.getAttribute('estado')) {
+        case 'i':
+            botaoPlay.setAttribute('estado', 'p')
+            botaoPlay.innerHTML = 'stop';
+        break;
+        case 'p':
+            botaoPlay.setAttribute('estado', 'i')
+            botaoPlay.innerHTML = 'play';
+        break;
+    }
 }
